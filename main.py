c47b30022ab8176ca4fac9f9a24a5882c02b0e81
Dishanth Arya <45206538+dish04@users.noreply.github.com>
2023-12-26 18:59:41 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,99 @@
+import imghdr
+import os
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.layers import (Conv2D, Dense, Dropout, Flatten,
+                                     MaxPooling2D)
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+#Limiting RAM
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)#Useless for me
+
+#Loading data
+data_train = './data/train'
+data_test = './data/test'
+
+data_gen = ImageDataGenerator(rescale=1./255)
+
+train = data_gen.flow_from_directory(data_train, class_mode = 'binary')
+test = data_gen.flow_from_directory(data_test, class_mode = 'binary')
+
+
+#Defining first model with 2 hidden layers with 128 nodes each
+#model1 = tf.keras.models.Sequential()
+
+#model1.add(tf.keras.layers.Flatten())
+#model1.add(tf.keras.layers.Dense(128, activation=tf.nn.relu)) #Adding first hidden layer which is using 128 neurons and ReL_U as activation
+#model1.add(tf.keras.layers.Dense(128, activation=tf.nn.relu)) #Adding second hidden layer which is using 128 neurons and ReL_U as activation
+#model1.add(tf.keras.layers.Dense(2, activation=tf.nn.softmax)) #Output layer with 2 outputs i.e. male or female
+
+#Saving model1
+#model1.compile(
+#    optimizer='adam',
+#    loss=tf.losses.BinaryCrossentropy(),
+#    metrics=['accuracy']
+#    )
+
+#history = model1.fit(train , epochs=10, validation_data = test)
+#model1.save("model1")
+
+#Model2
+model2 = Sequential()
+
+model2.add(Conv2D(16, (3,3), 1, activation='relu', input_shape=(256,256,3)))
+model2.add(MaxPooling2D())
+model2.add(Conv2D(32, (3,3), 1, activation='relu'))
+model2.add(MaxPooling2D())
+model2.add(Conv2D(16, (3,3), 1, activation='relu'))
+model2.add(MaxPooling2D())
+model2.add(Flatten())
+model2.add(Dense(256, activation='relu'))
+model2.add(Dense(1, activation='sigmoid'))
+
+#Saving model2
+model2.compile(
+    optimizer='adam',
+    loss=tf.losses.BinaryCrossentropy(),
+    metrics=['accuracy']
+    )
+
+history = model2.fit(train , epochs=10, validation_data = test)
+model2.save("model2")
+
+#Choosing model
+model = tf.keras.models.load_model('model2')
+model.compile(
+    optimizer='adam',
+    loss=tf.losses.BinaryCrossentropy(),
+    metrics=['accuracy']
+    )
+
+history = model.fit(train , epochs=10, validation_data = test)
+
+#Testing or predicting using the model
+loss, accuracy = model.evaluate(test)
+print(f'Validation Accuracy: {accuracy}')
+
+#Loading model1 to predict
+predictions = model.predict(test)
+
+fig = plt.figure()
+plt.plot(history.history['loss'], color='teal', label='loss')
+plt.plot(history.history['val_loss'], color='orange', label='val_loss')
+fig.suptitle('Loss', fontsize=20)
+plt.legend(loc="upper left")
+plt.show()
+
+fig = plt.figure()
+plt.plot(history.history['accuracy'], color='teal', label='accuracy')
+plt.plot(history.history['val_accuracy'], color='orange', label='val_accuracy')
+fig.suptitle('Accuracy', fontsize=20)
+plt.legend(loc="upper left")
+plt.show()
